94aa9617a42a8f3601741e1a3943f7125c2854d0
tianheg <yidajiabei@163.com>
2022-06-10 13:10:02 +0800

    add timeline-charts

---
+++ timeline-charts/README.md
@@ -0,0 +1,3 @@
+# MongoDB Charts
+
+https://www.mongodb.com/developer/products/atlas/build-animated-timeline-chart-embedding-sdk/
---
+++ timeline-charts/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Dashboard from "./Dashboard.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
